9354058f2d40f906524332e4b0b9be9dc67b8d5d
neyyzen <68753989+gHuseyinabi@users.noreply.github.com>
2021-02-19 22:57:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+BaseURL = 'https://www.quotes.net/mquote/'
+BaseFile = 'reffrences.html'
+Space = '<br>'
+Split = '<hr>'
+REFFRENCE_LIMIT = 10000
+
+FileStream = open(BaseFile, 'w')
+
+for i in range(3000, REFFRENCE_LIMIT):
+    urlstream = urlopen('{0}{1}'.format(BaseURL,i))
+    content = urlstream.read()
+    urlstream.close()
+    Soup = BeautifulSoup(content)
+    if i == 0:
+        Styles = Soup.find_all('link')
+        FileStream.write('\n'.join(str(match) for match in Soup.find_all("link")))
+    Quote = Soup.find("div",{"class":"disp-mquote-int"})
+    MovieName = Soup.find("h1",{"class":"movie-title"})
+    FileStream.write(str(MovieName) + Space + str(Quote) + Split)
+    print(MovieName)
+    print("Now at index {}".format(i))
