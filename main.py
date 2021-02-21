d317f919914e2d10a09affe11cad91e29824d6fb
neyyzen <68753989+gHuseyinabi@users.noreply.github.com>
2021-02-21 20:01:51 +0300

    Performance

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-
+import requests
+import thrading
 BaseURL = 'https://www.quotes.net/mquote/'
 BaseFile = 'reffrences.html'
+UserAgent = 'hi dont mind me im downloading movie quotes to use it offline thank'
 Space = '<br>'
 Split = '<hr>'
-
 ReffrenceLimit = 10_000
 Start = 0
 
 FileStream = open(BaseFile, 'w')
+Session = requests.Session()
+Session.headers.update({'User-Agent':UserAgent})
 
-for i in range(Start,ReffrenceLimit):
-    urlstream = urlopen('{0}{1}'.format(BaseURL,i))
-    content = urlstream.read()
-    urlstream.close()
+QuoteDict = {"class":"disp-mquote-int"}
+MovieDict = {"class":"movie-title"}
+
+def Look(i):
+    urlstream = Session.get('{0}{1}'.format(BaseURL,i))
+    content = urlstream.text
     Soup = BeautifulSoup(content)
     if i == 0:
         Styles = Soup.find_all('link')
         FileStream.write('\n'.join(str(match) for match in Soup.find_all("link")))
-    Quote = Soup.find("div",{"class":"disp-mquote-int"})
-    MovieName = Soup.find("h1",{"class":"movie-title"})
+    Quote = Soup.find("div",QuoteDict)
+    MovieName = Soup.find("h1",MovieDict)
     FileStream.write(str(MovieName) + Space + str(Quote) + Split)
     print(MovieName)
     print("Now at index {}".format(i))
+for i in range(Start,ReffrenceLimit):
+    threading.Thread(target=Look,args=(i,)).start()
+
